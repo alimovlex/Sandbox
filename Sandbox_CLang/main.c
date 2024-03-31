a2291f05f49ce22171d47257523e6722789e1425
alimovlex <alimovlex@hotmail.com>
2024-04-01 01:16:46 +0200

    Commented the bluetooth library section in CMake file for building the project in the cloud.

--- Sandbox_CLang/main.c
+++ Sandbox_CLang/main.c
@@ -6,13 +6,6 @@
     Copyright (c) 2020 alimovlex. All rights reserved.
 */
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <stdarg.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <Block.h> //lambda functions Apple extension
 #include "Object.h"
 #include "Tutorial.h"
 #include "ForwardList.h"
@@ -24,31 +17,14 @@
 #include "Queue.h"
 #include "tcp_server.h"
 #include "tcp_client.h"
-#include "bluetooth-test.h"
+//#include "bluetooth-test.h"
 #include "Multithread.h"
 #include "STLTest.h"
 #include "t_clib.h"
 #include "c_lib.h"
 #include "STLArray.h"
 
-
-int main(int argc, char *argv[])
-{   /*
-    int(*func)() = Multithreaded_Data_Structures_Test;
-    timerFunction(func);
-    func = Multithreaded_Tutorial_Functions_Test;
-    timerFunction(func);
-    */
-    //system("systemctl status bluetooth | grep -oh \"active\"");
-    list_bluetooth_devices();
-    //STL_Test();
-    return 0;
-}
-//switch((int)(n1 > n2))  
-//case 1 ... 5:
-
-/*
- //Test of starting myStartupFun function before main
+//Test of starting myStartupFun function before main
 void myStartupFun (void) __attribute__ ((constructor));
 void myCleanupFun (void) __attribute__ ((destructor));
 void myStartupFun (void) //make it multithreaded
@@ -61,4 +37,22 @@ void myCleanupFun (void)
 {
     printf ("\ncleanup code after main()\n");
 }
- */
+
+
+int main(int argc, char *argv[])
+{
+    int(*func)() = Multithreaded_Data_Structures_Test;
+    timerFunction(func);
+    func = Multithreaded_Tutorial_Functions_Test;
+    timerFunction(func);
+
+    //system("systemctl status bluetooth | grep -oh \"active\"");
+    //list_bluetooth_devices();
+    //STL_Test();
+    return 0;
+}
+//switch((int)(n1 > n2))  
+//case 1 ... 5:
+
+
+
